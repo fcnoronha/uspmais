68717f18f5f806287f783221adf975ed0f99958d
danigfavero <danifavero@usp.br>
2019-05-09 16:51:47 -0300

    Return to previous Org Controller

--- app/controllers/org_controller.rb
+++ app/controllers/org_controller.rb
@@ -1,4 +1,18 @@
 class OrgController < ApplicationController
-  def index
-  end
+
+  private
+
+    def org_params
+        params
+        .require(:org) # filters
+        .permit(:nome,
+                :email,
+                :instituto,
+                :curso,
+                :info,
+                :foto,
+                :link_site,
+                :link_fb,
+                :link_tt)
+    end
 end
