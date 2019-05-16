3d9d0a81f3e7d6b4ffe8beef387488ce98ab4cdd
rodrigovidotti <rodrigo.vidotti4@gmail.com>
2019-05-16 16:08:20 -0300

    Event table created with new,show and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
+
   # get 'user/index', to 'user#index' (Same as bellow)
   get 'user/index'
   get 'org/index'
+  get 'event/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Automatically defining basic routes for user
   resources :user
   resources :org
+  resources :event
 
   # Its a vision to application root
   # get '/', to 'user#index' (Same as bellow)
