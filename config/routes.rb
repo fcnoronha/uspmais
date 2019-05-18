e67cf41d4bed8613ff779fa938e77714c3d80a6e
mrfelipenoronha <felipe.castro.noronha@hotmail.com>
2019-05-18 12:00:59 -0300

    Static-pages new controller and better integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+  get 'static_pages/about'
   # get 'user/index', to 'user#index' (Same as bellow)
   get 'user/index'
   get 'org/index'
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
 
   # Its a vision to application root
   # get '/', to 'user#index' (Same as bellow)
-  root 'user#index'
+  root 'static_pages#home'
 
   # Login routes
   get    '/login',   to: 'session#new'
